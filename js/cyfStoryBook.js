a322f391281d15b2b8d87d55514e9a3d58cd1ddb
Pedro Ricciardi <riccip@outlook.com>
2023-08-30 10:26:14 +0100

    Story Book project added to My Work

---
+++ js/cyfStoryBook.js
@@ -0,0 +1,134 @@
+document.getElementById("sb__start").addEventListener("click", (event) => {
+    document.querySelectorAll(".sb__visible").forEach((selectedElement) => {
+      selectedElement.setAttribute("class", "sb__hidden");
+    });
+    let element_page1 = document.getElementById("sb__page1");
+    element_page1.setAttribute("class", "sb__visible");
+  });
+  
+  document.getElementById("sb__restart").addEventListener("click", (event) => {
+    document.querySelectorAll(".sb__visible").forEach((selectedElement2) => {
+      selectedElement2.setAttribute("class", "sb__hidden");
+    });
+    let element_title = document.getElementById("sb__title");
+    element_title.setAttribute("class", "sb__visible");
+    let element_instructions = document.getElementById("sb__instructions");
+    element_instructions.setAttribute("class", "sb__visible");
+    let element_start = document.getElementById("sb__start");
+    element_start.setAttribute("class", "sb__visible");
+  });
+  
+  document.getElementById("sb__toPage7-2").addEventListener("click", (event) => {
+    document.querySelectorAll(".sb__visible").forEach((selectedElement3) => {
+      selectedElement3.setAttribute("class", "sb__hidden");
+    });
+    let element_page7 = document.getElementById("sb__page7");
+    element_page7.setAttribute("class", "sb__visible");
+    let element_restart = document.getElementById("sb__restart");
+    element_restart.setAttribute("class", "sb__visible");
+  });
+  
+  document.getElementById("sb__toPage6").addEventListener("click", (event) => {
+    document.querySelectorAll(".sb__visible").forEach((selectedElement4) => {
+      selectedElement4.setAttribute("class", "sb__hidden");
+    });
+    let element_page6 = document.getElementById("sb__page6");
+    element_page6.setAttribute("class", "sb__visible");
+  });
+  
+  document.getElementById("sb__toPage2").addEventListener("click", (event) => {
+    document.querySelectorAll(".sb__visible").forEach((selectedElement5) => {
+      selectedElement5.setAttribute("class", "sb__hidden");
+    });
+    let element_page2 = document.getElementById("sb__page2");
+    element_page2.setAttribute("class", "sb__visible");
+  });
+  
+  document.getElementById("sb__toPage7").addEventListener("click", (event) => {
+    document.querySelectorAll(".sb__visible").forEach((selectedElement6) => {
+      selectedElement6.setAttribute("class", "sb__hidden");
+    });
+    let element_page72 = document.getElementById("sb__page7");
+    element_page72.setAttribute("class", "sb__visible");
+    let element_restart2 = document.getElementById("sb__restart");
+    element_restart2.setAttribute("class", "sb__visible");
+  });
+  
+  document.getElementById("sb__toPage7-3").addEventListener("click", (event) => {
+    document.querySelectorAll(".sb__visible").forEach((selectedElement7) => {
+      selectedElement7.setAttribute("class", "sb__hidden");
+    });
+    let element_page73 = document.getElementById("sb__page7");
+    element_page73.setAttribute("class", "sb__visible");
+    let element_restart3 = document.getElementById("sb__restart");
+    element_restart3.setAttribute("class", "sb__visible");
+  });
+  
+  document.getElementById("sb__toPage3").addEventListener("click", (event) => {
+    document.querySelectorAll(".sb__visible").forEach((selectedElement8) => {
+      selectedElement8.setAttribute("class", "sb__hidden");
+    });
+    let element_page3 = document.getElementById("sb__page3");
+    element_page3.setAttribute("class", "sb__visible");
+  });
+  
+  document.getElementById("sb__toPage8").addEventListener("click", (event) => {
+    document.querySelectorAll(".sb__visible").forEach((selectedElement9) => {
+      selectedElement9.setAttribute("class", "sb__hidden");
+    });
+    let element_page8 = document.getElementById("sb__page8");
+    element_page8.setAttribute("class", "sb__visible");
+  });
+  
+  document.getElementById("sb__toPage4").addEventListener("click", (event) => {
+    document.querySelectorAll(".sb__visible").forEach((selectedElement10) => {
+      selectedElement10.setAttribute("class", "sb__hidden");
+    });
+    let element_page4 = document.getElementById("sb__page4");
+    element_page4.setAttribute("class", "sb__visible");
+  });
+  
+  document.getElementById("sb__toPage8-2").addEventListener("click", (event) => {
+    document.querySelectorAll(".sb__visible").forEach((selectedElement11) => {
+      selectedElement11.setAttribute("class", "sb__hidden");
+    });
+    let element_page82 = document.getElementById("sb__page8");
+    element_page82.setAttribute("class", "sb__visible");
+  });
+  
+  document.getElementById("sb__toPage4-2").addEventListener("click", (event) => {
+    document.querySelectorAll(".sb__visible").forEach((selectedElement12) => {
+      selectedElement12.setAttribute("class", "sb__hidden");
+    });
+    let element_page42 = document.getElementById("sb__page4");
+    element_page42.setAttribute("class", "sb__visible");
+  });
+  
+  document.getElementById("sb__toPage9").addEventListener("click", (event) => {
+    document.querySelectorAll(".sb__visible").forEach((selectedElement13) => {
+      selectedElement13.setAttribute("class", "sb__hidden");
+    });
+    let element_page9 = document.getElementById("sb__page9");
+    element_page9.setAttribute("class", "sb__visible");
+    let element_restart4 = document.getElementById("sb__restart");
+    element_restart4.setAttribute("class", "sb__visible");
+  });
+  
+  document.getElementById("sb__toPage5").addEventListener("click", (event) => {
+    document.querySelectorAll(".sb__visible").forEach((selectedElement14) => {
+      selectedElement14.setAttribute("class", "sb__hidden");
+    });
+    let element_page5 = document.getElementById("sb__page5");
+    element_page5.setAttribute("class", "sb__visible");
+  });
+  
+  document.getElementById("sb__toPage10").addEventListener("click", (event) => {
+    document.querySelectorAll(".sb__visible").forEach((selectedElement15) => {
+      selectedElement15.setAttribute("class", "sb__hidden");
+    });
+    let element_page10 = document.getElementById("sb__page10");
+    element_page10.setAttribute("class", "sb__visible");
+    let element_restart5 = document.getElementById("sb__restart");
+    element_restart5.setAttribute("class", "sb__visible");
+  });
+  
